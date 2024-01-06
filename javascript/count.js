63d12d4621634f0fa37e7ecad1ef9ddce2f4ace1
Mikey-Xt-30 <mikeyxt30@gmail.com>
2024-01-06 10:25:04 +0530

    add

---
+++ javascript/count.js
@@ -0,0 +1,36 @@
+const para = document.getElementsByTagName("p");
+let count = 0;
+function incr() {
+  if (count < 15) {
+    count++;
+    para[0].textContent = count;
+    para[1].textContent = "increased";
+  } else {
+    alert("you can't increase than 15😝");
+  }
+}
+function decr() {
+  if (count > 0) {
+    count--;
+    para[0].textContent = count;
+    para[1].textContent = "decreased";
+  } else {
+    alert("don't decrease than zero😒");
+  }
+}
+function reset() {
+  let count = 0;
+  para[0].textContent = count;
+  para[1].textContent = "";
+}
+
+place = 0;
+function change() {
+  // color list
+  colorList = ["white", "black"];
+  // set the color
+  document.body.style.backgroundColor = colorList[place];
+  place++;
+  // if place is greater than the list size, reset
+  if (place === colorList.length) place = 0;
+}
